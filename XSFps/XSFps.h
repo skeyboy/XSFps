5e7fb7bd426eca68d248686930415e682a7259fc
le <le@ledemacbook-pro.local>
2018-04-12 13:22:25 +0800

    Initial Commit

---
+++ XSFps/XSFps.h
@@ -0,0 +1,19 @@
+//
+//  XSFps.h
+//  XSFps
+//
+//  Created by le on 2018/4/12.
+//  Copyright © 2018年 le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XSFps.
+FOUNDATION_EXPORT double XSFpsVersionNumber;
+
+//! Project version string for XSFps.
+FOUNDATION_EXPORT const unsigned char XSFpsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XSFps/PublicHeader.h>
+
+
